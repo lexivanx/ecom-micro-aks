8df0ff723880e2b2441275dd37940d54ec88e7b1
Alexander Ivanov <alexander.ivov.ivanov@proton.me>
2023-06-22 15:58:22 +0300

    Add exception handling, single product endpoint, logging

--- product-service/app.py
+++ product-service/app.py
@@ -15,16 +15,35 @@ class Product(db.Model):
 
 @app.route('/products', methods=['GET'])
 def get_products():
-    products = Product.query.all()
-    return jsonify([str(product) for product in products])
+    try:
+        products = Product.query.all()
+        return jsonify([str(product) for product in products])
+    except Exception as e:
+        return jsonify({'message': str(e)}), 500
+
+@app.route('/products/<int:product_id>', methods=['GET'])
+def get_product(product_id):
+    try:
+        product = Product.query.get(product_id)
+        if product:
+            return jsonify(str(product))
+        else:
+            return jsonify({'message': 'Product not found'}), 404
+    except Exception as e:
+        return jsonify({'message': str(e)}), 500
 
 @app.route('/products', methods=['POST'])
 def add_product():
     data = request.get_json()
-    new_product = Product(name=data['name'], inventory=data['inventory'])
-    db.session.add(new_product)
-    db.session.commit()
-    return jsonify({'message': 'New product added'}), 201
+    if not data or not data.get('name') or not data.get('inventory'):
+        return jsonify({'message': 'Bad Request'}), 400
+    try:
+        new_product = Product(name=data['name'], inventory=data['inventory'])
+        db.session.add(new_product)
+        db.session.commit()
+        return jsonify({'message': 'New product added'}), 201
+    except Exception as e:
+        return jsonify({'message': str(e)}), 500
 
 if __name__ == '__main__':
     db.create_all()  # Creates database tables
