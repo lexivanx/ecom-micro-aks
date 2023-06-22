0753999cd27574fa515341419c647dbfc508727f
Alexander Ivanov <alexander.ivov.ivanov@proton.me>
2023-06-22 15:57:56 +0300

    Add exception handling, single user endpoint, hased pswd, logging

--- user-service/app.py
+++ user-service/app.py
@@ -1,30 +1,52 @@
 from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'  # Replace this with your Azure SQL Database URI
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'  # Replace with Azure SQL Database URI
 db = SQLAlchemy(app)
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
+    password_hash = db.Column(db.String(128))
 
     def __repr__(self):
         return '<User %r>' % self.username
 
 @app.route('/users', methods=['GET'])
 def get_users():
-    users = User.query.all()
-    return jsonify([str(user) for user in users])
+    try:
+        users = User.query.all()
+        return jsonify([str(user) for user in users])
+    except Exception as e:
+        return jsonify({'message': str(e)}), 500
+
+@app.route('/users/<int:user_id>', methods=['GET'])
+def get_user(user_id):
+    try:
+        user = User.query.get(user_id)
+        if user:
+            return jsonify(str(user))
+        else:
+            return jsonify({'message': 'User not found'}), 404
+    except Exception as e:
+        return jsonify({'message': str(e)}), 500
 
 @app.route('/users', methods=['POST'])
 def add_user():
     data = request.get_json()
-    new_user = User(username=data['username'], email=data['email'])
-    db.session.add(new_user)
-    db.session.commit()
-    return jsonify({'message': 'New user added'}), 201
+    if not data or not data.get('username') or not data.get('email') or not data.get('password'):
+        return jsonify({'message': 'Bad Request'}), 400
+    try:
+        hashed_password = generate_password_hash(data['password'])
+        new_user = User(username=data['username'], email=data['email'], password_hash=hashed_password)
+        db.session.add(new_user)
+        db.session.commit()
+        return jsonify({'message': 'New user added'}), 201
+    except Exception as e:
+        return jsonify({'message': str(e)}), 500
 
 if __name__ == '__main__':
     db.create_all()  # Creates the database tables
